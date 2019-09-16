5568c7c1e2a36e5bd31e27344cb572cb14518774
gil kimpo <gilkimpo@hotmail.com>
2019-09-15 21:31:34 -0400

    this is also it

--- CurrencyDeliverable1/CurrencyDeliverable1/Program.cs
+++ CurrencyDeliverable1/CurrencyDeliverable1/Program.cs
@@ -3,63 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
-namespace CurrencyDeliverable1
+namespace System.Globalization
 {
     class Program
     {
         static void Main(string[] args)
         { 
-        double number1, number2, number3, average, swedish, japanese, thai ;
-            Console.WriteLine("Input your first positive value :");
+        double number1, number2, number3, total, average  ;
+            Console.WriteLine("Please enter your first value");
             number1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Input your second positive value :");
             number2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Input your third positive value :");
             number3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            total = Convert.ToDouble(number1 + number2 + number3);
             average = Convert.ToDouble((number1 + number2 + number3) / 3);
-            swedish = (Convert.ToDouble((9.68 * number1)));//FIX THIS!!!!!
-            japanese = (Convert.ToDouble(107.78*number2));///FIX THIS!!!
-            thai = (Convert.ToDouble(30.58*number3));///FIXTHIS
+            
+            
+            Console.WriteLine("The average of your values is : " );
+            Console.WriteLine(average.ToString("C"));
 
-            Console.WriteLine("The average of your values is : " + average);
+            Console.WriteLine("");
+
+
             {
 
-                if (number1 > number2)
+                if (number1 > number3 && number3 > number2)
                 {
-                    if (number1 > number3)
-                    {
-                        Console.WriteLine("The first value is the greatest amount " + number1);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The third value is the greatest amount " + number3);
-                    }
-                }
-                else if (number2 > number3)
-                {
-                    Console.WriteLine("The second value is the greatest amount " + number2);
-                }
-                if (number1 < number2)
-                {
-                    if (number1 < number3)
-                    {
-                        Console.WriteLine("The first value is the smallest amount " + number1);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The third value is the smallest amount" + number3);
-                    }
-                }
-                else if (number2 < number3)
-                {
-                    Console.WriteLine("The second value is the smallest amount" + number2);
-                }
+
+
+                    Console.WriteLine("The first value is the greatest amount  ");
+
+                    Console.WriteLine("The second value is the smallest  ");
 
                 }
+                if (number1 > number2 && number2> number3)
+                {
+                    Console.WriteLine("The first value is the greatest amount  ");
+
+                    Console.WriteLine("The third value is the smallest amount  ");
+
+                }
+
+                if (number2 > number1 && number1 > number3)
+                {
+                    Console.WriteLine("The second value is the greatest amount  ");
+                    Console.WriteLine("The third value is the smallest");
+
+
+                }
+                if(number2>number3 &&number3>number1)
+                { Console.WriteLine("The second value is the greatest amount  ");
+                    Console.WriteLine("The first value is the smallest");
+                }
+                if (number3>number1&&number1>number2)
+                {
+                    Console.WriteLine("The third value is the greatest amount ");
+                    Console.WriteLine("The second value is the smallest amount");
+                }
+                if (number3 > number2 && number2 > number1)
+                {
+                    Console.WriteLine("The third value is the greatest amount");
+                    Console.WriteLine("The first value is the lowest amount");
+                }
+                {
+                    Console.WriteLine("");
+                }
+
+
+                Console.WriteLine("");
+                Console.WriteLine("Your total value in USD is:");
+                Console.WriteLine(total.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("");
+                Console.WriteLine("Your total value in Japanese yen is:");
+                Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+                Console.WriteLine("");
+                Console.WriteLine("Your total value in the Swedish krona is:");
+                Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")));
+                Console.WriteLine("");
+                Console.WriteLine("Your total in the Thailand baht is:");
+                Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));
                 Console.ReadKey();
 
-            
+            }
         }
     }
 }
